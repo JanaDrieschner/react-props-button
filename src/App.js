2f2068f66b9464f6e1418934d843cb09221a1d76
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-03-05 17:08:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,5 +2,30 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    alert("You clicked a button");
+  }
+  return (
+    <div>
+      <Button color="green" text="click Me" onClick={handleClick} />
+      <Button color="red" disabled text="click Me" onClick={handleClick} />
+      <Greeting name={{ name: "Jana" }} />
+    </div>
+  );
+}
+
+function Button({ color, text, disabled, onClick }) {
+  return (
+    <button
+      style={{ backgroundColor: color }}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+function Greeting({ name }) {
+  <h1> Hello {"Jana"} </h1>;
 }
